e319edca5c32ff1486ec53a1a8ea30e9e3ba4743
veip007 <12257917+veip007@users.noreply.github.com>
2024-09-24 14:09:39 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,44 +20,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>{children}</body>
       <GoogleAnalytics gaId="G-JVKEXR5XSG" />
-    <style>
-        #overlay {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background: rgba(0, 0, 0, 0.8);
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            z-index: 1000;
-        }
-        #passwordForm {
-            text-align: center;
-            background: white;
-            padding: 20px;
-            border-radius: 10px;
-        }
-    </style>
-<body>
-    <div id="overlay">
-        <div id="passwordForm">
-            <h2>请输入密码</h2>
-            <input type="password" id="passwordInput" placeholder="Password">
-            <button onclick="checkPassword()">提交</button>
-        </div>
-    </div>
-    <script>
-        function checkPassword() {
-            var password = document.getElementById('passwordInput').value;
-            if (password === "521024") {
-                document.getElementById('overlay').style.display = 'none';
-            } else {
-                window.location.href = "/404";
-            }
-        }
-    </script>
 </body>
     </html>
   );
