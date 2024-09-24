9f3e3adbcd05a4df23cdb2e3ec4189fcbf010c8c
veip007 <12257917+veip007@users.noreply.github.com>
2024-09-24 13:42:18 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,8 +9,8 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "图床",
-  description: "图床",
+  title: "乐福图床",
+  description: "乐福图床",
 };
 
 
